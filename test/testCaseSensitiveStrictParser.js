9fb6081319c35d4cdaa5c23b9ce0d4d0b9fd20f7
harshad <harshad@in-harshad.local>
2018-01-08 19:03:41 +0530

    Completely got rid of node.js-assert and implemented chai-assert

--- test/testCaseSensitiveStrictParser.js
+++ test/testCaseSensitiveStrictParser.js
@@ -5,7 +5,7 @@ const Parsed=require(src('parsed.js'));
 const StrictParser=require(src('index.js')).StrictParser;
 
 describe("strict parser that is case insensitive",function(){
-  it("should parse when specified keys are in lower case and actual is not",function(){
+  it("should parse when specified key are in lower case and actual is not",function(){
     let kvParser=new StrictParser(["name"],false);
     // false indicates that case sensitive is false. By default it is true
     let expected=new Parsed();
@@ -13,7 +13,38 @@ describe("strict parser that is case insensitive",function(){
     let parsed=kvParser.parse("NAME=jayanth");
     assert.deepEqual(parsed,expected);
   });
+  it("should parse when specified key are in both lower case as well as upper case and actual is not",function(){
+    let kvParser=new StrictParser(["name"],false);
+    let expected=new Parsed();
+    expected["Name"]="jayanth";
+    let parsed=kvParser.parse("Name=jayanth");
+    assert.deepEqual(parsed,expected);
+  });
+  it("should parse when specified key combined of alphabets and numbers are in both case, but actual is in lower case",function(){
+    let kvParser=new StrictParser(["name123"],false);
+    let expected=new Parsed();
+    expected["Name123"]="jayanth";
+    let parsed=kvParser.parse("Name123=jayanth");
+    assert.deepEqual(parsed,expected);
+  });
+  it("should parse when specified key and actual key is combined of alphabets and number",function(){
+    let kvParser=new StrictParser(["name123"],false);
+    let expected=new Parsed();
+    expected["Name123"]="jayanth";
+    let parsed=kvParser.parse("Name123=jayanth");
+    assert.deepEqual(parsed,expected);
+  });
+  it("should parse when specified key has both alphabets, number and symbol in it and actual alphabets are in lower case",function(){
+    let kvParser=new StrictParser(["name_123"],false);
+    let expected=new Parsed();
+    expected["Name_123"]="jayanth";
+    let parsed=kvParser.parse("Name_123=jayanth");
+    assert.deepEqual(parsed,expected);
+  });
 });
+
+
+
 
 describe("strict parser that is case sensitive",function(){
   it("should throw error when specified keys are in lower case and actual is not",function(){
--- test/testStrictParser.js
+++ test/testStrictParser.js
@@ -1,6 +1,7 @@
 const src=function(filePath){return "../src/"+filePath};
 const errors=function(filePath){return "../src/errors/"+filePath};
-const assert = require('assert');
+const assert = require('chai').assert;
+const Parsed = require(src('parsed'));
 const StrictParser=require(src('index.js')).StrictParser;
 const InvalidKeyError=require(errors('invalidKeyError.js'));
 
@@ -18,74 +19,113 @@ describe("strict parser",function(){
     assert.throws(
       () => {
         var p=kvParser.parse("age=23");
-      } ,
-      invalidKeyErrorChecker("age",5)()
-  )});
+      } ,InvalidKeyError);
+      try{
+        var p=kvParser.parse("age=23");
+      }catch(e){
+        assert.ok(invalidKeyErrorChecker("age",5)(e));
+      }
+  });
 
   it("should only parse keys that are specified for multiple keys",function(){
     let kvParser=new StrictParser(["name","age"]);
     let actual=kvParser.parse("name=john age=23");
-    let expected={name:"john",age:"23"};
+    let expected = new Parsed();
+    expected['name']="john";
+    expected['age']="23";
     assert.deepEqual(expected,actual);
     assert.throws(
       () => {
-        var p=kvParser.parse("color=blue");
-       },
-      invalidKeyErrorChecker("color",9)
-  )});
-
+        kvParser.parse("color=blue");
+      },InvalidKeyError);
+      try{
+        kvParser.parse("color=blue");
+      }catch(e){
+        assert.ok(invalidKeyErrorChecker("color",9)(e));
+      }
+  });
   it("should throw an error when one of the keys is not valid",function(){
     assert.throws(
       () => {
         let kvParser=new StrictParser(["name","age"]);
         kvParser.parse("name=john color=blue age=23");
-      },
-      invalidKeyErrorChecker("color",20)
-  )});
+      },InvalidKeyError);
+      try{
+        let kvParser=new StrictParser(["name","age"]);
+        kvParser.parse("name=john color=blue age=23");
+      }catch(e){
+        assert.ok(invalidKeyErrorChecker("color",20)(e));
+      }
+  });
 
   it("should throw an error on invalid key when there are spaces between keys and assignment operators",function(){
     assert.throws(
       () => {
         let kvParser=new StrictParser(["name","age"]);
         kvParser.parse("color   = blue");
-      },
-      invalidKeyErrorChecker("color",13)
-  )});
+      },InvalidKeyError);
+      try{
+        let kvParser=new StrictParser(["name","age"]);
+        kvParser.parse("color   = blue");
+      }catch(e){
+        assert.ok(invalidKeyErrorChecker("color",13)(e));
+      }
+  });
 
   it("should throw an error on invalid key when there are quotes on values",function(){
     assert.throws(
       () => {
         let kvParser=new StrictParser(["name","age"]);
         kvParser.parse("color   = \"blue\"");
-      },
-      invalidKeyErrorChecker("color",15)
-  )});
+      },InvalidKeyError);
+      try{
+        let kvParser=new StrictParser(["name","age"]);
+        kvParser.parse("color   = \"blue\"");
+      }catch(e){
+        assert.ok(invalidKeyErrorChecker("color",15)(e));
+      }
+  });
 
   it("should throw an error on invalid key when there are cases of both quotes and no quotes",function(){
     assert.throws(
       () => {
         let kvParser=new StrictParser(["name","age"]);
         kvParser.parse("name = john color   = \"light blue\"");
-       },
-      invalidKeyErrorChecker("color",33)
-  )});
+       },InvalidKeyError);
+      try{
+        let kvParser=new StrictParser(["name","age"]);
+        kvParser.parse("name = john color   = \"light blue\"");
+       }catch(e){
+          assert.ok(invalidKeyErrorChecker("color",33)(e));
+       }
+  });
 
   it("should throw an error when no valid keys are specified",function(){
     assert.throws(
       () => {
         let kvParser=new StrictParser([]);
         kvParser.parse("name=john");
-      },
-      invalidKeyErrorChecker("name",8)
-  )});
+      },InvalidKeyError);
+      try{
+        let kvParser=new StrictParser([]);
+        kvParser.parse("name=john");
+      }catch(e){
+        assert.ok(invalidKeyErrorChecker("name",8)(e));
+      }
+  });
 
   it("should throw an error when no array is passed",function(){
     assert.throws(
       () => {
         let kvParser=new StrictParser();
         kvParser.parse("name=john");
-       },
-      invalidKeyErrorChecker("name",8)
-  )});
-
+      },InvalidKeyError
+    );
+    try{
+      let kvParser=new StrictParser();
+      kvParser.parse("name=john");
+    }catch(e){
+      assert.ok(invalidKeyErrorChecker("name",8)(e));
+    }
+  });
 });
